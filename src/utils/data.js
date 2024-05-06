146f90ec178ec8ba48c4571f5cd9a1d61a89c7a1
pallavi777706 <prateekdubey19999@gmail.com>
2024-05-06 16:25:19 +0530

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,122 @@
+export const SKILLS=[
+    {
+        title:"Frontend",
+        icon:"./assets/images/fr1.png",
+        skills:[
+            {skill:"HTML5",percentage:"95%"},
+            {skill:"CSS3",percentage:"80%"},
+            {skill:"JavaScript",percentage:"75%"},
+            {skill:"React Js",percentage:"70%"},
+        ],
+    },
+    {
+        title:"Backend",
+        icon:"./assets/images/b1.png",
+        skills:[
+            {skill:"Node.js",percentage:"50%"},
+            {skill:"Java",percentage:"75%"},
+            {skill:"SpringBoot",percentage:"60%"},
+            
+        ],  
+    },
+    {
+        title:"Tools",
+        icon:"./assets/images/tool1.png",
+        skills:[
+            {skill:"Git & GitHub",percentage:"75%"},
+            {skill:"Visual Studio Code",percentage:"80%"},
+            {skill:"Responsive Design",percentage:"60%"},
+            
+        ],  
+    },
+    {
+        title:"Cloud",
+        icon:"./assets/images/a4.png",
+        skills:[
+            {skill:"AWS",percentage:"60%"},
+           
+            
+        ],  
+    },
+    {
+        title:"Soft Skills",
+        icon:"./assets/images/s2.png",
+        skills:[
+            {skill:"Problem-Solving",percentage:"80%"},
+            {skill:"Collaboration",percentage:"85%"},
+            {skill:"Attention to Detail",percentage:"75%"},
+            
+        ],  
+    },
+
+];
+
+export const WORK_EXPERIENCE=[
+    {
+        title:"Associate Software Engineer At Mphasis",
+        date:"june 2022 - april 2024",
+        responsibilities:
+        [
+            "Contribute to the development of an application designed to assist visitors seeking Indian visas abroad and obtaining visa related services within India during their stay. ",
+            "Primarily involved in the development of the user interface using React.js, HTML, CSS, and TypeScript."
+
+        ],
+    },
+    {
+        title:"Intern at Dysmech Competency Services PVT.LTD",
+        date:"may 2022 - june 2022",
+        responsibilities:
+        [
+            "Acquired hands-on experience working with various sensors,micro-controllers, and boards, gaining valuable insights into IoT technologies ",
+            "Developed a strong foundation in teamwork skills and gained exposure to the dynamics of the corporate world during the internship."
+
+        ],
+    },
+    
+];
+
+export const Project_Experience=[
+    {
+        title:"Visa Service",
+        date:"November 2023 - Present",
+        responsibilities:
+        [
+            "his project facilitates visitors seeking Indian Visa and provides visa related services during their stay. Primarily focused on UI development for the project ",
+        
+            "Utilized Technologies :React,js Html5 & Css ,Typescript",
+        ],
+    },
+    {
+        title:"Onine Food Delivery System",
+        date:"october 2022 - october 2022",
+        responsibilities:
+        [
+            "Developed a comprehensive online food services website Utilized cutting-edge technologies, including Spring Boot for Java-based services ",
+            "Implemented Angular for the user interface to enhance user experience. ",
+        ],
+    },
+    {
+        title:"Smart Automatic Accident Detection Information System",
+        date:"September 2021 - April 2022",
+        responsibilities:
+        [
+            "This project focused on Accident Detection, incorporating a feature to assess driver sobriety. Ability to achieve precise identification of accident locations. Recieved IEEE recognition for project innovation and acceptance for publication.",
+            "Technologies: IoT Sensors, Arduino. ",
+           
+        ],
+    },
+    {
+        title:"Smart Car Parking System ",
+        date:"February 2021 - April 2021",
+        responsibilities:
+        [
+          "Engineered an efficient vehicle parking system enabling drivers to easily find available parking spaces. Integrated IR sensors to detect parking slot occupancy and utilized a servo motor for gate simulation. ", 
+          "Applied Technologies: Python, OpenCV, IoT sensors. ",
+        ],
+    },
+    
+
+    
+    
+
+];
